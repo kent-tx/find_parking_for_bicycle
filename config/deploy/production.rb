24c44e3758bf1f9c547c75f709b8e65f11633a4d
kent-tx <siroitakenoko@gmail.com>
2021-05-05 01:33:30 +0900

    add

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 server '52.199.172.154', user: 'kento', roles: %w{app db web}
-set :ssh_options, keys: '~/.ssh/my-instance-key1.pem'
+set :ssh_options, keys: '~/.ssh/my-insta ce-key1.pem'
 # set :ssh_options, keys: '~/.ssh/instance1_key_rsa'
