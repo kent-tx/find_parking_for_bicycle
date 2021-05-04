8c6c3212211fef8595e42dc0399fa3ece9d6fa1c
kent-tx <siroitakenoko@gmail.com>
2021-05-05 01:24:30 +0900

    add cap

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 server '52.199.172.154', user: 'kento', roles: %w{app db web}
-set :ssh_options, keys: '~/.ssh/aws_git_rsa'
+set :ssh_options, keys: '~/.ssh/instance_key_rsa'
 # set :ssh_options, keys: '~/.ssh/instance1_key_rsa'
