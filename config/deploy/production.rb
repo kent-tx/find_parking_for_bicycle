6fb219b5223c4a147192c02a691ece8633bfec99
kent-tx <siroitakenoko@gmail.com>
2021-05-05 13:42:22 +0900

    add

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 server '52.199.172.154', user: 'kento', roles: %w{app db web}
 set :ssh_options, keys: '~/.ssh/instance1_key_rsa'
-set :ssh_options, keys: '~/.ssh/my-instance-key1.pem'
+# set :ssh_options, keys: '~/.ssh/my-instance-key1.pem'
 # set :ssh_options, keys: '~/.ssh/instance1_key_rsa.pub'
 # set :ssh_options, keys: '~/.ssh/instance1_key_rsa'
